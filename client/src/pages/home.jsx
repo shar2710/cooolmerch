a7cab4495ff71819fc5379b63231b508322cad8c
shar2710 <s.sejal2710@gmail.com>
2024-10-22 02:07:36 +0530

    kkkkkk

---
+++ client/src/pages/home.jsx
@@ -0,0 +1,10 @@
+import {motion, AnimatePresence} from 'framer-motion'
+import {useSnapshot} from 'valtio'
+import {headContainerAnimation, headContentAnimation, haedTextAnimation, slideAnimation} from '../config/motion'
+const home = () => {
+  return (
+    <div>home</div>
+  )
+}
+
+export default home
