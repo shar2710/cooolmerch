edfd2acc196f08cfc62b034f95c5c15c399e8071
shar2710 <s.sejal2710@gmail.com>
2024-10-30 14:42:29 +0530

    ade

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import CustomButton from "./custombutton";
+import AIPicker from "./aipicker";
+import ColorPicker from "./colorpicker";
+import FilePicker from "./filepicker";
+import Tab from "./tab";
+
+export { 
+  CustomButton,
+  AIPicker,
+  ColorPicker,
+  FilePicker,
+  Tab,
+};
